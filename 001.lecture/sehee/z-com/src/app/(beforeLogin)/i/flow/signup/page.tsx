36846cee7edd291843f720fb18b567c3d40419e4
김세희 <sadie1001@naver.com>
2024-01-13 18:05:58 +0900

    세희 섹션1 완료

---
+++ 001.lecture/sehee/z-com/src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -0,0 +1,5 @@
+import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
+
+export default function Signup() {
+  return <SignupModal />;
+}
---
+++ 001.lecture/sehee/z-com/src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Main from "@/app/(beforeLogin)/_component/Main";
+
+export default function Login() {
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return <Main />;
+}
